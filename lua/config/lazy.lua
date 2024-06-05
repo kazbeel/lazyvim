64bbdaf5982f9be2bd3b61e9f2029f10cc6376ef
Diego Valladolid <d.valladolid@factor-eleven.com>
2024-06-05 10:17:39 +0200

    lazy: add mini-indentscope as extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,9 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
 
     { import = "lazyvim.plugins.extras.formatting.prettier" },
 
