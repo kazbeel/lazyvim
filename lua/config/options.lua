9ebacc8afbf9535191c8bc30786da99665a2218b
Diego Valladolid <d.valladolid@factor-eleven.com>
2024-06-05 17:27:38 +0200

    trouble: disable trouble component in lualine

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.opt.undodir = "/tmp"
+
+-- Disable Trouble component in lualine
+vim.g.trouble_lualine = false
