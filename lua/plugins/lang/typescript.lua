791421f984f8b2b2f1cfc580cb73e5303984af7d
Diego Valladolid <d.valladolid@factor-eleven.com>
2024-07-12 19:02:23 +0200

    neotest: remove all repeated keymaps

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -18,63 +18,42 @@ return {
   },
   keys = {
     {
-      "<leader>tl",
-      function()
-        require("neotest").run.run_last()
-      end,
-      desc = "Run Last Test",
-    },
-    {
       "<leader>tc",
       function()
         require("neotest").run.run()
       end,
-      desc = "Run Test Case",
+      desc = "Run Case",
     },
-    {
-      "<leader>tf",
-      function()
-        require("neotest").run.run(vim.fn.expand("%"))
-      end,
-      desc = "Run Test File",
-    },
-    {
-      "<leader>tw",
-      function()
-        local function fileExists(filename)
-          local file = io.open(filename, "r")
-          if file then
-            io.close(file)
-            return true
-          else
-            return false
-          end
-        end
-
-        local testCommand
-
-        if fileExists("vitest.config.ts") then
-          local vitestCommand = "vitest --watch"
-          testCommand = { vitestCommand = vitestCommand }
-        else
-          local jestCommand = require("neotest-jest.jest-util").getJestCommand(vim.fn.expand("%:p:h"))
-          testCommand = { jestCommand = jestCommand .. " --watch" }
-        end
-
-        require("neotest").run.run({
-          vim.fn.expand("%"),
-          testCommand,
-          suite = true,
-        })
-      end,
-      desc = "Test Watch",
-    },
-    {
-      "<leader>tt",
-      function()
-        require("neotest").summary.toggle()
-      end,
-      desc = "Test Summary (Toggle)",
-    },
+    -- {
+    --   "<leader>tw",
+    --   function()
+    --     local function fileExists(filename)
+    --       local file = io.open(filename, "r")
+    --       if file then
+    --         io.close(file)
+    --         return true
+    --       else
+    --         return false
+    --       end
+    --     end
+    --
+    --     local testCommand
+    --
+    --     if fileExists("vitest.config.ts") then
+    --       local vitestCommand = "vitest --watch"
+    --       testCommand = { vitestCommand = vitestCommand }
+    --     else
+    --       local jestCommand = require("neotest-jest.jest-util").getJestCommand(vim.fn.expand("%:p:h"))
+    --       testCommand = { jestCommand = jestCommand .. " --watch" }
+    --     end
+    --
+    --     require("neotest").run.run({
+    --       vim.fn.expand("%"),
+    --       testCommand,
+    --       suite = true,
+    --     })
+    --   end,
+    --   desc = "Test Watch",
+    -- },
   },
 }
