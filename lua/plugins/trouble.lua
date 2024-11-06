d4e33bff805ad20624aa85d2c41da11191244422
Diego Valladolid <d.valladolid@factor-eleven.com>
2024-11-06 08:34:32 +0100

    trouble: move preview to a right window

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,5 +2,11 @@ return {
   "folke/trouble.nvim",
   opts = {
     focus = true,
+    preview = {
+      type = "split",
+      relative = "win",
+      position = "right",
+      size = 0.4,
+    },
   },
 }
