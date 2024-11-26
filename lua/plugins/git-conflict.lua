92132aa7e9358a061d62784466311557d548db46
Diego Valladolid <d.valladolid@factor-eleven.com>
2024-11-26 13:54:37 +0100

    git-conflict: remap keys

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,18 +1,18 @@
 return {
   "akinsho/git-conflict.nvim",
   version = "*",
+  config = true,
   lazy = false,
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("git-conflict").setup({
-      default_mappings = {
-        ours = "co",
-        theirs = "ct",
-        none = "c0",
-        both = "cb",
-        next = "]x",
-        prev = "[x",
-      },
-    })
-  end,
+  opts = {
+    default_mappings = false,
+  },
+  keys = {
+    { "<leader>gCo", "<cmd>GitConflictChooseOurs<cr>", desc = "Select the current changes" },
+    { "<leader>gCt", "<cmd>GitConflictChooseTheirs<cr>", desc = "Select the incoming changes" },
+    { "<leader>gCb", "<cmd>GitConflictChooseBoth<cr>", desc = "Select both changes" },
+    { "<leader>gC0", "<cmd>GitConflictChooseNone<cr>", desc = "Select none of the changes" },
+    { "<leader>gCq", "<cmd>GitConflictListQf<cr>", desc = "Open conflicts in quickfix" },
+    { "]x", "<cmd>GitConflictNextConflict<cr>", desc = "Move to the next conflict" },
+    { "[x", "<cmd>GitConflictPrevConflict<cr>", desc = "Move to the previous conflict" },
+  },
 }
