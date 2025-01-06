f32516f02a87d1fbea9aa9a5071c10b4a49c9055
kazbeel <diego.valladolid@gmail.com>
2025-01-06 12:08:12 +0100

    colorscheme: activate background transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "catppuccin",
+    lazy = false,
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
