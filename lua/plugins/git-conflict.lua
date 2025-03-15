b61dd283934a38469571c8c29a414ae1097171ac
Diego Valladolid <d.valladolid@factor-eleven.com>
2025-03-15 10:18:31 +0100

    git-conflict: add keymaps

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -9,7 +9,9 @@ return {
   keys = {
     { "<leader>gx", desc = "+Resolve git conflics" },
     { "<leader>gxo", "<cmd>GitConflictChooseOurs<cr>", desc = "Select the current changes" },
+    { "<leader>gxc", "<cmd>GitConflictChooseOurs<cr>", desc = "Select the current changes" },
     { "<leader>gxt", "<cmd>GitConflictChooseTheirs<cr>", desc = "Select the incoming changes" },
+    { "<leader>gxi", "<cmd>GitConflictChooseTheirs<cr>", desc = "Select the incoming changes" },
     { "<leader>gxb", "<cmd>GitConflictChooseBoth<cr>", desc = "Select both changes" },
     { "<leader>gx0", "<cmd>GitConflictChooseNone<cr>", desc = "Select none of the changes" },
     { "<leader>gxq", "<cmd>GitConflictListQf<cr>", desc = "Open conflicts in quickfix" },
